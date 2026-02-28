06a4a7dc362919d1cef850c5b6e98c2e3125048c
NathanBernard <nbx-off@outlook.fr>
2026-02-28 20:59:00 +0100

    Added functions logic to generate the pages

--- src/index.js
+++ src/index.js
@@ -1,25 +1,48 @@
 import "./style.css";
+import { generateMenuContent } from "./menu";
+import { generateAboutContent } from "./about";
 
+const buttons = document.querySelectorAll("button");
 const contentDiv = document.getElementById("content");
+const pages = {
+  home: generateIndexContent,
+  menu: generateMenuContent,
+  about: generateAboutContent,
+};
 
-// Hero
-const heroDiv = document.createElement("div");
-heroDiv.classList.add("hero");
+const generateIndexContent = function () {
+  // Hero
+  const heroDiv = document.createElement("div");
+  heroDiv.classList.add("hero");
 
-// Presentation
-const presentationDiv = document.createElement("div");
-presentationDiv.classList.add("presentation");
-const pureEleganceH2 = document.createElement("h2");
-pureEleganceH2.textContent = "pure elegance";
-presentationDiv.append(pureEleganceH2);
+  // Presentation
+  const presentationDiv = document.createElement("div");
+  presentationDiv.classList.add("presentation");
+  const pureEleganceH2 = document.createElement("h2");
+  pureEleganceH2.textContent = "pure elegance";
+  presentationDiv.append(pureEleganceH2);
 
-// Testimony
-const testimonyDiv = document.createElement("div");
-testimonyDiv.classList.add("testimony");
-const quoteText = document.createElement("blockquote");
-quoteText.textContent = "A better restaurant? Not in this world.";
-const citeText = document.createElement("cite");
-citeText.textContent = "Joe Biden";
-testimonyDiv.append(quoteText, citeText);
+  // Testimony
+  const testimonyDiv = document.createElement("div");
+  testimonyDiv.classList.add("testimony");
+  const quoteText = document.createElement("blockquote");
+  quoteText.textContent = "A better restaurant? Not in this world.";
+  const citeText = document.createElement("cite");
+  citeText.textContent = "Joe Biden";
+  testimonyDiv.append(quoteText, citeText);
 
-contentDiv.append(heroDiv, presentationDiv, testimonyDiv);
+  contentDiv.append(heroDiv, presentationDiv, testimonyDiv);
+};
+
+const clearContent = function () {
+  contentDiv.innerHTML = "";
+};
+
+generateIndexContent();
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const page = e.currentTarget.dataset.page;
+    pages[page]();
+  });
+});
